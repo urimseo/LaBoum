9ccc378ecf9212e87fad6bbdc0112f1f22331e2b
urimseo <urim960417@gmail.com>
2021-11-19 09:37:18 +0900

    1119_0937

--- movies/urls.py
+++ movies/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:movie_pk>/', views.detail, name='detail'),
     path('<int:genre_movie_pk>/genre_movie', views.detail2, name='detail2'),
     path('recommended/', views.recommended, name='recommended'),
+    path('color/', views.color, name='color'),
 ]
